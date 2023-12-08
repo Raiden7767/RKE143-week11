17060a52cebf4c51a82d0420c119432976346f4f
KirillSemasko <ksemasko@tktk.ee>
2023-12-09 01:34:25 +0200

    week 12

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const ingredientRouter = require('./routes/ingredients.routes');
 const fullRecipesRouter = require('./routes/fullRecipes.routes');
 const randomRouter = require('./routes/randomRecipe.routes');
 const app = express();
+app.use(express.json());
 
 
 app.use('/ingredients', ingredientRouter);
